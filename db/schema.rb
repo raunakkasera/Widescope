6c707c6d38326d3367d8c0598f0aa22d72f4a874
Raunak Kasera <raunak@cs.stanford.edu>
2016-06-10 22:25:47 -0700

    Adding annotation model...

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160503051228) do
+ActiveRecord::Schema.define(version: 20160611051629) do
+
+  create_table "annotations", force: :cascade do |t|
+    t.integer  "userId"
+    t.float    "dollarAmount"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "budgetposts", force: :cascade do |t|
     t.text     "content"
